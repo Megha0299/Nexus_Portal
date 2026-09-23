b4910556cc05d5e7e6799747d476a4c4ac5b86dd
Megha0299 <meghasureshkumar02@gmail.com>
2026-09-23 11:14:31 +0530

    fix: embed firebase config for inquiries collection

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyA_Zac0dyo05GPzid1jUu5jQrn0dGiGREQ",
+  authDomain: "roboweb-technologies.firebaseapp.com",
+  projectId: "roboweb-technologies",
+  storageBucket: "roboweb-technologies.firebasestorage.app",
+  messagingSenderId: "645143645827",
+  appId: "1:645143645827:web:f9a0250ba5249e22fe95b6",
+  measurementId: "G-CMZFMF4WZ7",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
